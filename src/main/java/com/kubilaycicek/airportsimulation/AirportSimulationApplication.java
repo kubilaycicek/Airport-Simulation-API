32c40986a24c3d842334122187b5fb8bb3e9799d
kubilaycicek <kubilaycicek0693@gmail.com>
2020-12-19 13:12:45 +0300

    init

---
+++ src/main/java/com/kubilaycicek/airportsimulation/AirportSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.kubilaycicek.airportsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubilaycicek/airportsimulation/AirportSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubilaycicek.airportsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
